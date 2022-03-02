fb09b3c1e13539dbc4013a09881a81dd194ffd9f
raj99-code <rajm7394@gmail.com>
2022-03-02 00:01:27 +0000

    Changed absolute paths to relative

--- scripts/detection.cpp
+++ scripts/detection.cpp
@@ -1,10 +1,16 @@
 #include "recog_model.h"
 
 int main() {
-   cv::Mat in_img = cv::imread("/home/pi/American-Sign-Language-Recognition-using-Deep-Neural-Network/images/sample_prediction (5).png");
    std::vector<std::string> classes = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"};
-   LetterRecog model("/home/pi/realtime-sign-language-recognition/model/asl_alphabet.onnx");
-   cv::Point2f forw = model.forward(in_img);
-   std::cout << "Detected class is: " << classes[forw.x] << std::endl;
+   for(std::string class_name : classes) {
+	   if (class_name == "del") { continue; }
+	   std::string path = "../imgs/" + class_name + "_test.jpg";
+	   cv::Mat in_img = cv::imread(path);
+	   LetterRecog model("../model/asl_alphabet.onnx");
+	   cv::Point2f forw = model.forward(in_img);
+	   std::cout << "Original class is: " << class_name << std::endl;
+	   std::cout << "Detected class is: " << classes[forw.x] << std::endl;
+	   std::cout << "\n" << std::endl;
+   }
    return 0;
 }
