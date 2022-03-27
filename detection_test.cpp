7aaebe9e9eb1cb5ac65acfdf9e0f381523b21e28
Abdullah-alfakhrey <alfakhrey.abdullah@gmail.com>
2022-03-27 21:54:11 +0100

    Black background fixed

--- detection_test.cpp
+++ detection_test.cpp
@@ -7,74 +7,20 @@
 #include <iostream>
 #include <stdio.h>
 #include "BackgroundRemover.h"
+#include "SkinDetector.h"
 
 using namespace cv;
 
 using namespace std;
 
 
-/*
- Author: Pierfrancesco Soffritti https://github.com/PierfrancescoSoffritti
-*/
-
-BackgroundRemover::BackgroundRemover(void) {
-	background;
-	calibrated = false;
-}
-
-void BackgroundRemover::calibrate(Mat input) {
-	cvtColor(input, background, COLOR_BGR2GRAY);
-	calibrated = true;
-}
-
-Mat BackgroundRemover::getForeground(Mat input) {
-	Mat foregroundMask = getForegroundMask(input);
-
-	//imshow("foregroundMask", foregroundMask);
-
-	Mat foreground;
-	input.copyTo(foreground, foregroundMask);
-
-	return foreground;
-}
-
-Mat BackgroundRemover::getForegroundMask(Mat input) {
-	Mat foregroundMask;
-
-	if (!calibrated) {
-		foregroundMask = Mat::zeros(input.size(), CV_8UC1);
-		return foregroundMask;
-	}
-
-	cvtColor(input, foregroundMask, COLOR_BGR2GRAY);
-
-	removeBackground(foregroundMask, background);
-	
-	return foregroundMask;
-}
-
-void BackgroundRemover::removeBackground(Mat input, Mat background) {
-	int thresholdOffset = 10;
-
-	for (int i = 0; i < input.rows; i++) {
-		for (int j = 0; j < input.cols; j++) {
-			uchar framePixel = input.at<uchar>(i, j);
-			uchar bgPixel = background.at<uchar>(i, j);
-
-			if (framePixel >= bgPixel - thresholdOffset && framePixel <= bgPixel + thresholdOffset)
-				input.at<uchar>(i, j) = 0;
-			else
-				input.at<uchar>(i, j) = 255;
-		}
-	}
-}
-
-BackgroundRemover backgroundRemover;
 int main() {
     
 Mat frame, frameOut, handMask, foreground, fingerCountDebug;    
 
 Mat image, HSV;
+BackgroundRemover backgroundRemover;
+SkinDetector skinDetector;
 
 namedWindow("Display window");
 
@@ -89,13 +35,19 @@ cout << "cannot open camera";
 while (true) {
 
 cap >> image;
+frameOut=image.clone();
+skinDetector.drawSkinColorSampler(frameOut);
+
 foreground = backgroundRemover.getForeground(image);
-imshow("foreground", foreground);
+handMask = skinDetector.getSkinMask(foreground);
+imshow("foreground1", frameOut);
+imshow("foreground2", foreground);
+imshow("foreground3", handMask);
 
 LetterRecog model("asl_alphabet.onnx");
 
 Point2f forw = model.forward(image);
-//imshow("Live", image);
+imshow("Live", image);
 cout << "Detected class is: " << forw.x << endl;
 cout << "\n" << endl;
 
@@ -106,7 +58,9 @@ if (key == 27) // esc
 	break;
 
 else if (key == 98) // b
-	backgroundRemover.calibrate(frame);
+	backgroundRemover.calibrate(image);
+else if (key == 115) // s
+	skinDetector.calibrate(image);
 
 }
 
