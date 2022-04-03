e2353e744d6cbe43ea71dfc1eaa920f31dacc117
raj99-code <80386115+raj99-code@users.noreply.github.com>
2022-04-03 13:18:14 +0100

    Edited indentation

--- scripts/webcam_detection.cpp
+++ scripts/webcam_detection.cpp
@@ -4,7 +4,7 @@ int main() {
 	cv::Mat curr_image;
 	cv::namedWindow("Display Window");
 	cv::VideoCapture cap(0);
-    std::vector<std::string> classes = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"};
+    	std::vector<std::string> classes = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"};
 	LetterRecog model("../model/asl_alphabet.onnx");
 	
 	if (!cap.isOpened()) { std::cout << "Unable to open camera" << std::endl; }
@@ -13,8 +13,8 @@ int main() {
 		cap >> curr_image;
 		cv::imshow("Display Window", curr_image);
 		cv::Point2f forw = model.forward(curr_image);
-	    std::cout << classes[forw.x] << std::endl;
-	    std::cout << "\n" << std::endl;
+	    	std::cout << classes[forw.x] << std::endl;
+	    	std::cout << "\n" << std::endl;
 		cv::waitKey(25);
 	}
 
